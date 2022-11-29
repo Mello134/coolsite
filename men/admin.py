84dd9162ec4431d9d370d6e1d9e9bb9a6d10a3fd
Mello134 <elproject220@yandex.ru>
2022-11-29 18:17:35 +0300

    '24'

--- men/admin.py
+++ men/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *  # импортируем все классы men/models.py
 
 
 class MenAdmin(admin.ModelAdmin):
     # список отображаемых столбцов в админ-панели
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_publisher')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_publisher')
     # список полей на которые можно кликнуть - для редактирования
     list_display_links = ('id', 'title')
     # список полей - ко которым можно вести поиск
@@ -14,7 +16,18 @@ class MenAdmin(admin.ModelAdmin):
     # фильтрование по полям - публикация, время изменения.
     list_filter = ('is_publisher', 'time_create')
     prepopulated_fields = {'slug': ('title',)}  # автозаполнение слага по имени поста
+    # список полей которые стоит отображать в форме редактирования
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_publisher', 'time_create', 'time_update')
+    # не редактируемые поля (только после этой строчки можно добавить из в fields)
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True  # панель сверху(дубль)
 
+    # отобажение миниматюр фото в админке
+    def get_html_photo(self, object):
+        if object.photo:  # если фото есть
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")  # mark_safe - не экранирует теги
+
+    get_html_photo.short_description = "Миниатюра"
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -28,3 +41,7 @@ class CategoryAdmin(admin.ModelAdmin):
 # регистрируем класс отображения в админке - MenAdmin
 admin.site.register(Men, MenAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+# заголовки в админке
+admin.site.site_title = 'Админ-панель Mello'
+admin.site.site_header = 'Админ-панель Mello'
